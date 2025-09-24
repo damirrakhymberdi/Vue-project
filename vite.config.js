c0696473c0e434b68066b62245f2f58336dee992
damirrahymberdi1289 <damir.rahymberdi@jihc.edu.kz>
2025-09-24 13:01:14 +0500

    🌐 Added GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: process.env.NODE_ENV === 'production' ? '/Vue-project/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
